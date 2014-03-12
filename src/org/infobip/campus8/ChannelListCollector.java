9b582cbab09e6695c77dbefc68470feb43969174
mobradovic <marko.obradovic@infobip.com>
2014-03-12 11:27:45 +0100

    Added ChannelListCollector and ChannelListJsonParser

--- src/org/infobip/campus8/ChannelListCollector.java
+++ src/org/infobip/campus8/ChannelListCollector.java
@@ -1,5 +1,76 @@
 package org.infobip.campus8;
 
-public class ChannelListCollector {
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+
+import android.content.Context;
+import android.net.ParseException;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.Toast;
+
+public class ChannelListCollector {
+	String channels = "";
+	
+	public List<String> getChannels() throws InterruptedException, ExecutionException, JSONException{
+		ChannelListTask task = new ChannelListTask();
+		ChannelListJsonParser parser = new ChannelListJsonParser();
+		channels = task.execute(new Void[0]).get();
+		List<String> channelList = parser.parse(channels);
+		return channelList;
+	}
+	
+	private class ChannelListTask extends AsyncTask<Void, Void, String>{
+
+		private final String HEADER_VALUE = "Basic cHVzaGRlbW86cHVzaGRlbW8=";
+		private final String HEADER_NAME = "Authorization";
+		private final String CHANNEL_LIST_URL = "https://pushapi.infobip.com/1/application/9cabf301d3db/channels";
+		
+		@Override
+		protected String doInBackground(Void...voids) {
+			HttpClient client = new DefaultHttpClient();
+			HttpGet get = new HttpGet(CHANNEL_LIST_URL);
+			get.setHeader(HEADER_NAME, HEADER_VALUE);
+			HttpResponse responseGet = null;
+			try {
+				responseGet = client.execute(get);
+			} catch (ClientProtocolException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}  
+	        HttpEntity resEntityGet = responseGet.getEntity();  
+	        String output = "";
+	        if (resEntityGet != null) {
+	        	try {
+					output = EntityUtils.toString(resEntityGet);
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+	        }
+			return output;
+		}
+		
+		@Override
+		protected void onPostExecute(String result){
+			Log.e("do in background:", "Poèetak on post exe");
+			channels += result;
+		}
+	}
 }
--- src/org/infobip/campus8/MainActivity.java
+++ src/org/infobip/campus8/MainActivity.java
@@ -1,26 +1,64 @@
 package org.infobip.campus8;
 
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.ParseException;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.fragment_main);
-
-
+		getList();
 	}
 
+	private void getList() {
+		if(isConnected()){
+			ChannelListCollector channelListCollector = new ChannelListCollector();
+			String channels;
+			try {
+				channels = (channelListCollector.getChannels()).toString();
+				Toast.makeText(getBaseContext(), channels, Toast.LENGTH_LONG).show();
+			} catch (InterruptedException e) {
+				Toast.makeText(getBaseContext(), "Exception during fetching channel list: " + e.toString(), Toast.LENGTH_SHORT).show();
+			} catch (ExecutionException e) {
+				Toast.makeText(getBaseContext(), "Exception during fetching channel list: " + e.toString(), Toast.LENGTH_SHORT).show();
+			} catch (JSONException e) {
+				Toast.makeText(getBaseContext(), "Error parsing channel list json: " + e.toString(), Toast.LENGTH_SHORT).show();
+			}
+		}
+		else{
+			 Toast.makeText(getBaseContext(), "You are not connected to the internet", Toast.LENGTH_SHORT).show();
+		}
+	}
+
+	private boolean isConnected() {
+		ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+		return (networkInfo != null && networkInfo.isConnected());
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 
