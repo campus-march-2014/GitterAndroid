3fa76bcbf34445676edd19a40005890e9b363c0b
Filip Potocki <fpotocki@infobip.com>
2014-03-13 12:15:40 +0100

    bins

---
+++ src/org/infobip/campus8/AboutActivity.java
@@ -0,0 +1,45 @@
+package org.infobip.campus8;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
+
+public class AboutActivity extends Activity {
+	
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+	// TODO Auto-generated method stub
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.about);
+}
+	
+@Override
+public boolean onCreateOptionsMenu(Menu menu) {
+menu.add(0, 1, 0, "Settings");
+menu.add(0, 2, 0, "About");
+ return true;
+}
+
+@Override
+public boolean onOptionsItemSelected(MenuItem item) {
+
+ switch (item.getItemId()) {
+ case 1: {
+  Intent intentSubsOne = new Intent(AboutActivity.this,PostRegistrationChannelSubscription.class);
+  startActivity(intentSubsOne);
+  break;
+ 	}
+ case 2: {
+	   Intent intentSubs = new Intent(AboutActivity.this,AboutActivity.class);
+	   startActivity(intentSubs);
+	   break;
+	  	}
+ }
+ return true;
+}
+}
